51a8dd5d67570a5ac5c039e07e4d58eff9451942
Sriprabha Gopalan <sgopala@L-SB8X2BJG8W-M.local>
2015-08-27 22:42:46 -0700

    Adding the basic dependeices

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Hackathon-webapp
 
-Ashwini test commit
+
+##To install node:
+
+https://nodejs.org/download/
+
+
+##To install bower using npm package manager. 
+
+sudo npm install -g bower 
+
+
+## Update dependencies
+npm install/npm update
+
+
+## Run the local web server
+node app.js
+
+## App is running on http://localhost:9898/
+
+
+
---
+++ app.js
@@ -0,0 +1,19 @@
+'use strict';
+
+
+var express = require('express'),
+	bodyParser = require('body-parser');
+
+var data = {},
+	id = 0,
+	app = express();
+
+//------------------------------------------------------------------------------
+// Server
+//------------------------------------------------------------------------------
+
+app.use(bodyParser());
+
+
+app.use(express.static(__dirname + '/public')).listen(9898);
+console.log('Started listening on port 9898');
